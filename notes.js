3d7edea5776fe3914e4438e33577e55f04811f6a
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-08-10 21:44:01 +0000

    fast_commit at 20210810_214401

--- notes.js
+++ notes.js
@@ -31,10 +31,84 @@ function readUntilFinish() {
   return result
 }
 
-(async () => {
-  let notes
-  let db
+// global variables
+var notes
+var db
 
+// program commands
+function helpCommand() {
+  console.log("Commands: \n" +
+    "new-\tCreate a Note\n" +
+    "del-\tDelete a Note\n" +
+    "mod-\tModify a Note\n" +
+    "list-\tList Notes\n" +
+    "print-\tPrint a Note to the screen\n" +
+    "exit-\tLeave the program.")
+}
+
+async function newCommand() {
+  const noteTitle = readlinesync.question("title: ")
+  const noteContents = readUntilFinish()
+
+  let note = new Note(noteTitle, noteContents)
+  notes[note.id] = note.title
+  try {
+    console.log("Saving note to the database... (1/2)")
+    await db.saveEntry(note.id, JSON.stringify(note))
+    console.log("Saving note list to the database... (2/2)")
+    await db.saveEntry("list", JSON.stringify(notes))
+    console.log("Done")
+  } catch (e) {
+    console.error(e)
+    return
+  }
+}
+
+function listCommand() {
+  if (Object.keys(notes).length > 0) {
+    for (let [key, value] of Object.entries(notes)) {
+      console.log("Id: " + key + "\nTitle:" + value)
+    }
+  } else {
+    console.log("There are not saved notes.")
+  }
+}
+
+async function printCommand(input) {
+  let inArr = input.split(" ")
+  if (inArr.length < 2 && inArr[1] === undefined) {
+    console.log("Usage: print <Note Id>")
+    return
+  }
+  let noteId = inArr[1].trim()
+  if (noteId == "") {
+    console.log("Invalid note Id.")
+    return
+  }
+
+  let idFound = false
+  for (let key of Object.keys(notes)) {
+    if (key == noteId) {
+      idFound = true
+      break
+    }
+  }
+  if (!idFound) {
+    console.log("The note does not exists.")
+    return
+  }
+  try {
+    let note = JSON.parse(await db.getEntry(noteId))
+    console.log("Title: " + note.title)
+    console.log("Creation time: " + note.creationTime)
+    console.log("Last modified: " + note.lastModified)
+    console.log(note.contents)
+  } catch (e) {
+    console.error("Cannot get note:\n" + e)
+  }
+}
+
+(async () => {
   try {
     db = new PepinoDB("http://localhost:50200", "notes-js", "caipiroska")
   } catch (e) {
@@ -47,43 +121,20 @@ function readUntilFinish() {
   } catch (e) {
     notes = {}
   }
+
   let exit = false
   console.log("Welcome to the Notes Program!\n" +
     "TIP: type help for the command list.")
   while (!exit) {
     const cmd = readlinesync.question("notes>")
     if (cmd.startsWith("help")) {
-      console.log("Commands: \n" +
-        "new-\tCreate a Note\n" +
-        "del-\tDelete a Note\n" +
-        "mod-\tModify a Note\n" +
-        "list-\tList Notes\n" +
-        "print-\tPrint a Note to the screen\n" +
-        "exit-\tLeave the program.")
+      helpCommand()
     } else if (cmd.startsWith("new")) {
-      const noteTitle = readlinesync.question("title: ")
-      const noteContents = readUntilFinish()
-
-      let note = new Note(noteTitle, noteContents)
-      notes[note.id] = note.title
-      try {
-        console.log("Saving note to the database... (1/2)")
-        await db.saveEntry(note.id, JSON.stringify(note))
-        console.log("Saving note list to the database... (2/2)")
-        await db.saveEntry("list", JSON.stringify(notes))
-        console.log("Done")
-      } catch (e) {
-        console.error(e)
-        return
-      }
+      await newCommand()
     } else if (cmd.startsWith("list")) {
-      if (Object.keys(notes).length > 0) {
-        for (let [key, value] of Object.entries(notes)) {
-          console.log("Id: " + key + "\nTitle:" + value)
-        }
-      } else {
-        console.log("There are not saved notes.")
-      }
+      listCommand()
+    } else if (cmd.startsWith("print")) {
+      await printCommand(cmd)
     } else if (cmd.startsWith("exit")) {
       console.log("Goodybye.")
       exit = true
